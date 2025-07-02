8125deae8e04674bce4a1e2d900113538e173ff4
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-07-02 11:37:42 +0530

    Add FileController class to manage file upload and download functionalities

---
+++ src/main/java/p2p/controller/FileController.java
@@ -0,0 +1,44 @@
+package p2p.controller;
+
+import p2p.service.FileSharer;
+
+import java.io.*;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.apache.commons.io.IOUtils;
+
+public class FileController {
+    private final FileSharer fileSharer;
+    private final HttpServer httpServer;
+    private final String uploadDir;
+    private final ExecutorService executorService;
+
+    public FileController(int port) throws IOException {
+        this.fileSharer = new FileSharer();
+        this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        this.uploadDir = System.getProperty("java.io.tmpdir") + File.separator + "peerlink-uploads";
+        this.executorService = Executors.newFixedThreadPool(10);
+        
+        File uploadDirFile = new File(uploadDir);
+        if (!uploadDirFile.exists()) {
+            uploadDirFile.mkdirs();
+        }
+        
+        server.createContext("/upload", new UploadHandler());
+        server.createContext("/download", new DownloadHandler());
+        server.createContext("/", new CORSHandler());
+        
+        server.setExecutor(executorService);
+    }
+
+}
