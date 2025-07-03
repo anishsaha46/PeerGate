335a16884fca622413d88f64b7ea2ee987e2199b
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-07-03 09:49:18 +0530

    Add MultipartParser class to FileController for handling multipart data parsing

--- src/main/java/p2p/controller/FileController.java
+++ src/main/java/p2p/controller/FileController.java
@@ -74,6 +74,71 @@ public class FileController {
     }
 
 
+        private static class MultipartParser {
+        private final byte[] data;
+        private final String boundary;
+        
+        public MultipartParser(byte[] data, String boundary) {
+            this.data = data;
+            this.boundary = boundary;
+        }
+        
+        public ParseResult parse() {
+            try {
+                String dataAsString = new String(data);
+                
+                String filenameMarker = "filename=\"";
+                int filenameStart = dataAsString.indexOf(filenameMarker);
+                if (filenameStart == -1) {
+                    return null;
+                }
+                
+                filenameStart += filenameMarker.length();
+                int filenameEnd = dataAsString.indexOf("\"", filenameStart);
+                String filename = dataAsString.substring(filenameStart, filenameEnd);
+                
+                String contentTypeMarker = "Content-Type: ";
+                int contentTypeStart = dataAsString.indexOf(contentTypeMarker, filenameEnd);
+                String contentType = "application/octet-stream"; // Default
+                
+                if (contentTypeStart != -1) {
+                    contentTypeStart += contentTypeMarker.length();
+                    int contentTypeEnd = dataAsString.indexOf("\r\n", contentTypeStart);
+                    contentType = dataAsString.substring(contentTypeStart, contentTypeEnd);
+                }
+                
+                String headerEndMarker = "\r\n\r\n";
+                int headerEnd = dataAsString.indexOf(headerEndMarker);
+                if (headerEnd == -1) {
+                    return null;
+                }
+                
+                int contentStart = headerEnd + headerEndMarker.length();
+                
+                byte[] boundaryBytes = ("\r\n--" + boundary + "--").getBytes();
+                int contentEnd = findSequence(data, boundaryBytes, contentStart);
+                
+                if (contentEnd == -1) {
+                    boundaryBytes = ("\r\n--" + boundary).getBytes();
+                    contentEnd = findSequence(data, boundaryBytes, contentStart);
+                }
+                
+                if (contentEnd == -1 || contentEnd <= contentStart) {
+                    return null;
+                }
+                
+                byte[] fileContent = new byte[contentEnd - contentStart];
+                System.arraycopy(data, contentStart, fileContent, 0, fileContent.length);
+                
+                return new ParseResult(filename, contentType, fileContent);
+            } catch (Exception e) {
+                System.err.println("Error parsing multipart data: " + e.getMessage());
+                return null;
+            }
+        }
+
+
+
     private class UploadHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
